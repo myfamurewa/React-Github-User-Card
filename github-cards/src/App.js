1c3cb2dcf5d6525fb1a6f9c394dbc80c6ca024ac
viscountfam <V3ctor42!>
2020-01-14 17:45:41 -0500

    I just have to style. basic functionality is complete

---
+++ github-cards/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from './logo.svg';
+import {render} from 'react-dom'
+import axios from 'axios'
+import GitHubCard from './components/GitHubCard'
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      follower: []
+    }
+  }
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/viscountfam`)
+      .then(response => {
+        console.log("User Response", response)
+        this.setState({
+          user: response.data
+        })
+      })
+      .catch(err => {
+        console.log("An error has occurred", err)
+      })
+    axios.get('https://api.github.com/users/viscountfam/followers')
+    .then(response => {
+      console.log("follower initial response", response);
+      response.data.forEach(item => {
+        axios.get( `https://api.github.com/users/${item.login}`)
+      .then(res => {
+      console.log("follower full response set", this.state.follower);
+      this.setState({
+        follower: [...this.state.follower, res.data]
+      })
+    });
+      })
+    })
+  }
+
+  render() {
+  return (
+    <div>
+    <GitHubCard user={this.state.user} />
+    {this.state.follower.map(user => (
+      <GitHubCard key={user.id} user={user} />
+    ))}
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ github-cards/src/components/GitHubCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function GitHubCard(props) {
+    return (
+        <div className="card">
+            <img src={props.user.avatar_url} alt={`${props.user.name}'s profile picture`} />
+            <div className="card-info">
+                <h3 className="name">{props.user.name}</h3>
+                 <p className="username">{props.user.login}</p>
+                 <p>Location: {props.user.location}</p>
+                 <p>Github: <a href={props.user.html_url}>{props.user.login}</a></p>
+                 <p>Followers:{props.user.followers}</p>
+                 <p>Following:{props.user.following}</p>
+                 <p>{props.bio}</p>
+            </div>
+            <img src={`http://ghchart.rshah.org/${props.user.login}`} alt={`${props.user.name}'s GitHub chart`}></img>
+        </div>
+    )
+}
