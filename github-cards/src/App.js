6ae353abb7bfd5c1a37dd349d75e703eb7171002
viscountfam <V3ctor42!>
2020-01-15 10:01:11 -0500

    Stretch goal completed

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import {render} from 'react-dom'
 import axios from 'axios'
 import GitHubCard from './components/GitHubCard'
 import './App.css';
@@ -10,7 +8,10 @@ class App extends React.Component {
     super();
     this.state = {
       user: [],
-      follower: []
+      follower: [],
+      userText: "",
+      guestUser: [],
+      guestfollower: []
     }
   }
   componentDidMount() {
@@ -39,13 +40,52 @@ class App extends React.Component {
     })
   }
 
+  findUser = e => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.userText}`)
+    .then(res =>
+      this.setState({
+        guestUser: res.data
+      })
+      )
+      axios.get(`https://api.github.com/users/${this.state.userText}/followers`)
+    .then(response => {
+      console.log("guest follower initial response", response);
+      response.data.forEach(item => {
+        axios.get( `https://api.github.com/users/${item.login}`)
+      .then(res => {
+      console.log(" guest follower full response set", this.state.follower);
+      this.setState({
+        guestfollower: [...this.state.guestfollower, res.data]
+      })
+    });
+      })
+    })
+  }
+
+  handleChanges = e => {
+    this.setState({
+      userText: e.target.value
+    });
+  };
+
   render() {
   return (
     <div className="container">
+      <input
+          type="text"
+          value={this.state.userText}
+          onChange={this.handleChanges}
+        />
+      <button onClick={this.findUser}>find user</button>
     <div className="cards">
     <GitHubCard user={this.state.user} />
+    <GitHubCard className="guest" user={this.state.guestUser} />
     {this.state.follower.map(user => (
       <GitHubCard key={user.id} user={user} />
+    ))}
+    {this.state.guestfollower.map(user => (
+      <GitHubCard className="guest" key={user.id} user={user} />
     ))}
     </div>
     </div>
